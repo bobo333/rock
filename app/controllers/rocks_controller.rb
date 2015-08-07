46b3c13a228c1f37edfc97ee54df0208a044854d
bobo333 <redsox333@gmail.com>
2015-08-07 00:01:21 -0700

    Initial commit, basic configuration for devise and user creation

---
+++ app/controllers/rocks_controller.rb
@@ -0,0 +1,21 @@
+class RocksController < ApplicationController
+
+    before_action :authenticate_user!, only: [:new]
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+end
